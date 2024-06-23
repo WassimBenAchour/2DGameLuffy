7f5481e9069264c0aa11834f9819dc044d5f82c7
WassimBenAchour <medwassimbenachour@gmail.com>
2024-06-23 14:43:10 +0200

    Update Cube.cs

--- 2DLuffyGameFiles/Assets/Cube.cs
+++ 2DLuffyGameFiles/Assets/Cube.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Cube : MonoBehaviour
-{
+{   
+    string TheNameOfPlayer = "wassim";
+    int AgeOfPlayer=25;
+    float TallOfPlayer=1.74f; 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello everyone am printing from Debug");
-        Debug.LogWarning("Hello everyone am printing from Warning");
-        Debug.LogError("Hello everyone am printing from Error");
+        Debug.Log("Hello everyone the player Name is "+TheNameOfPlayer);
+        Debug.LogWarning("Hello everyone the player Age is "+AgeOfPlayer);
+        Debug.LogError("Hello everyone the player tall is "+TallOfPlayer);
     }
 
     // Update is called once per frame
