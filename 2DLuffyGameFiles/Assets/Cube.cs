0affeb6e4e2a23ad818f25d6f33157e00f6b01dd
WassimBenAchour <medwassimbenachour@gmail.com>
2024-06-23 13:22:31 +0200

    Added LogDiff messages
    Debug.Log
    Debug.LogWarning
    Debug.LogError

---
+++ 2DLuffyGameFiles/Assets/Cube.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello everyone am printing from Debug");
+        Debug.LogWarning("Hello everyone am printing from Warning");
+        Debug.LogError("Hello everyone am printing from Error");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
