8ee1d02eaa9337f0fc4ed87fe6000a9ea8d30daa
WassimBenAchour <medwassimbenachour@gmail.com>
2024-06-24 23:12:11 +0200

    add a fonctions with public and void returns

--- 2DLuffyGameFiles/Assets/Cube.cs
+++ 2DLuffyGameFiles/Assets/Cube.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class Cube : MonoBehaviour
 {
@@ -8,20 +9,26 @@ public class Cube : MonoBehaviour
     string TheNameOfPlayer = "wassim";
     int AgeOfPlayer=25;
     float TallOfPlayer=1.74f; 
-    // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello everyone the player Name is "+TheNameOfPlayer);
-        Debug.LogWarning("Hello everyone the player Age is "+AgeOfPlayer);
-        Debug.LogError("Hello everyone the player tall is "+TallOfPlayer);
+        PrintingToOurConsol();
     }
 
-    // Update is called once per frame
- void Update()
+    void Update()
+    {
+        OutOfBoundsPrinter();
+        MovingOurCube();
+    }
+    public void PrintingFromOutside()
+    {
+        Debug.Log(" Hello From the Other side!! ");
+    }
+
+    private void MovingOurCube()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            rigidbody.velocity = new Vector2(0f, 10f)
+            rigidbody.velocity = new Vector2(0f, 10f);
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
@@ -35,5 +42,35 @@ public class Cube : MonoBehaviour
         {
             rigidbody.velocity = new Vector2(10f, 0f);
         }
+    }
+
+    private void OutOfBoundsPrinter()
+    {
+        if (transform.position.x > 9.4f)
+        {
+            Debug.Log("our cube is out of bounds to the Right side!");
+            Debug.Log(PositionCube(transform.position.x, "Right"));
+        }
+        if (transform.position.x < -9.4f)
+        {
+            Debug.Log("our cube is out of bounds to the Left side!");
+            Debug.Log(PositionCube(transform.position.x, "Left"));
+        }
+        if (transform.position.y > 5.6f)
+        {
+            Debug.Log("our cube is out of bounds to the Top side!");
+            Debug.Log(PositionCube(transform.position.y, "Top"));
+        }
+    }
+    public string PositionCube(float X,string Pos)
+    {
+        return ("Your position in "+Pos+" is "+ X);
+    }
+
+    private void PrintingToOurConsol()
+    {
+        Debug.Log("Hello everyone the player Name is " + TheNameOfPlayer);
+        Debug.LogWarning("Hello everyone the player Age is " + AgeOfPlayer);
+        Debug.LogError("Hello everyone the player tall is " + TallOfPlayer);
     }
 }
